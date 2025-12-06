89081618da5d479579416c1cd058b1d75a21af00
Vlad-Nic <vladjnic04@gmail.com>
2025-12-06 01:18:13 -0500

    Merge branch 'Fades'

--- Scripts/Door.cs
+++ Scripts/Door.cs
@@ -3,33 +3,33 @@ using System;
 
 public partial class Door : StaticBody2D
 {
-    [Export] public AnimatedSprite2D DoorSprite;
+	[Export] public AnimatedSprite2D DoorSprite;
 
-    private CollisionShape2D _collisionShape;
-    private Area2D _triggerArea;
+	private CollisionShape2D _collisionShape;
+	private Area2D _triggerArea;
 
-    public override void _Ready()
-    {
-        _collisionShape = GetNodeOrNull<CollisionShape2D>("CollisionShape2D");
-        _triggerArea = GetNodeOrNull<Area2D>("TriggerArea");
+	public override void _Ready()
+	{
+		_collisionShape = GetNodeOrNull<CollisionShape2D>("CollisionShape2D");
+		_triggerArea = GetNodeOrNull<Area2D>("TriggerArea");
 
-        if (_triggerArea != null)
-            _triggerArea.BodyEntered += OnBodyEntered;
-    }
+		if (_triggerArea != null)
+			_triggerArea.BodyEntered += OnBodyEntered;
+	}
 
-    public void PlayOpenAnimation()
-    {
-        DoorSprite?.Play("Open");
-        if (_collisionShape != null)
-            _collisionShape.Disabled = true;
-    }
+	public void PlayOpenAnimation()
+	{
+		DoorSprite?.Play("Open");
+		if (_collisionShape != null)
+			_collisionShape.Disabled = true;
+	}
 
-    private void OnBodyEntered(Node2D body)
-    {
-        if (body is Player)
-        {
-            GD.Print("Player entered door area → loading next room");
-            RandomSceneLoader.Instance?.LoadNextRoom();
-        }
-    }
+	private void OnBodyEntered(Node2D body)
+	{
+		if (body is Player)
+		{
+			GD.Print("Player entered door area → loading next room");
+			RandomSceneLoader.Instance?.LoadNextRoom();
+		}
+	}
 }
